dd5628083b4166030652f86ec4692c3ccb26093d
Rtunes <Rtunes@MacBook-Pro-de-Ricardo.local>
2019-08-23 20:38:48 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let button = document.querySelector(".randomWordButton");
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let wordList = document.querySelector("#generatedWordsList");
+let splittedAlphabet = alphabet.split("");
+button.addEventListener("click", event => {
+  let arrayWordGenerated = [];
+  let wordListItem = document.createElement("li");
+  for (let i = 0; i < 10; i++) {
+    let randomNumber = Math.floor(Math.random() * 25);
+    let randomLetter = splittedAlphabet[randomNumber];
+    arrayWordGenerated.push(randomLetter);
+  }
+  let wordGenerated = arrayWordGenerated.join("");
+  wordListItem.innerHTML = wordGenerated;
+  wordList.appendChild(wordListItem);
+});
